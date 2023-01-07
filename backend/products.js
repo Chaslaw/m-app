5cfdce5ac18b95a86d77a7905125fae31e908c0b
Caslav Lazic <caslavlazic@gmail.com>
2023-01-07 21:23:21 +0100

    first commit

---
+++ backend/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id: 1,
+        name: 'iPhone 12 PRO',
+        description: '6.1 inch', 
+        price: 666,
+        img: 'https://res.cloudinary.com/dwwgnkz4g/image/upload/v1670844168/mobile/iphone12pro_fpggeg.jpg'
+    },
+    {
+        id: 2,
+        name: 'iPhone 12',
+        description: '3.3 inch', 
+        price: 690,
+        img: 'https://res.cloudinary.com/dwwgnkz4g/image/upload/v1670844169/mobile/iphone12_yp7wmv.jpg'
+    },
+    {
+        id: 3,
+        name: 'Samsung Galaxy',
+        description: '5.6 inch', 
+        price: 580,
+        img: 'https://res.cloudinary.com/dwwgnkz4g/image/upload/v1670844169/mobile/galaxyS_czdm6y.png'
+    }
+];
+
+module.exports = products;
+
+// export default products;
